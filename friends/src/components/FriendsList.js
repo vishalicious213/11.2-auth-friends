392bef3739d4cd4ce3dd161b0df5e68a3a2de2b6
vishalicious213 <vishalicious213@gmail.com>
2020-05-10 13:34:43 -0400

    fighting with state in FriendsList. I can console.log the results from the axios call and see them but I can't set those results to state.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -8,20 +8,29 @@ const FriendsList = () => {
     useEffect(() => {
         // axios
         // .get('http://localhost:5000/api/friends', {headers: {Authorization: localStorage.getItem('token')}})
-        console.log('From FriendsList: ', localStorage.getItem('token'));
+        // console.log('From FriendsList: ', localStorage.getItem('token'));
         axiosWithAuth()
             .get('http://localhost:5000/api/friends')
-            .then(results => {
-                console.log(results.data);
-                setFriends(results.data);
-            })
+            .then(results => console.log('API: ', results.data))
+            .then(results => setFriends(results.data))
+            .then(console.log('FriendsList: ', friends))
+            // .then(results => {
+            //     console.log(results.data);
+            //     setFriends(results.data);
+            //     console.log('FriendsList: ', friends)
+            // })
             .catch(error => {
                 console.log('Error: ', error);
-            })
-    },[]);
+            });
+    },[friends]);
 
     return (
-        <div>Friends List</div>
+        // friends.map(friend => (
+        //     <h3>friend.name</h3>
+        // ))
+        // <section className='friendslist'>
+            <div>Friends List</div>
+        // </section>
     )
 }
 
